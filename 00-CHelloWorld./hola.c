1938beddecf2697c4caae310b8fc718d923f54db
LautaroDevcpp <149957240+LautaroDevcpp@users.noreply.github.com>
2024-04-10 10:47:55 -0300

    saqué el fprintf
    lo redireccioné por consola usando ./a > helloWord.txt

--- 00-CHelloWorld./hola.c
+++ 00-CHelloWorld./hola.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
- 
+
 int main()
 {
-    // using printf to print "Hello Geek!"
-
-    FILE* f = fopen("HelloWord.txt","a");
-
-    fprintf(f,"Hello, Lautaro!");
+    printf("Hello, Lautaro!!");
 
     return 0;
 }
